cedb95a066c99411d45babfb417f16a8e048418c
Nikolay <nikolaynazarovvv216@gmail.com>
2023-03-31 21:34:04 +0300

    first commit

---
+++ movie_app/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.urls import reverse
+
+
+# Create your models here.
+class Movie(models.Model):
+    name = models.CharField(max_length=40)
+    rating = models.IntegerField()
+    year = models.IntegerField(null=True)
+    budget = models.IntegerField(default=1000000)
+
+    def get_url(self):
+        return reverse('movie-detail', args=[self.id])
+
+    def __str__(self):
+        return f'{self.name} - {self.rating}% {self.year}'
