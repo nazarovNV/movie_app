d9eb0f1c1a27d3a3edcfe75b3b9e2f806a9b358b
Nikolay <nikolaynazarovvv216@gmail.com>
2023-04-02 18:50:16 +0300

    finished 3.14

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Movie
+from django.db.models import F, Sum, Max, Min, Count, Avg
 
 
 # from movie_app.models import Movie
@@ -8,14 +9,17 @@ from .models import Movie
 
 
 def show_all_movie(request):
-    movies = Movie.objects.all()
+    movies = Movie.objects.order_by(F('year').asc(nulls_last=True), 'rating')
+    agg = movies.aggregate(Avg("budget"), Max("rating"), Min("rating"), Max("year"))
     return render(request, 'movie_app/all_movies.html', {
-        'movies': movies
+        'movies': movies,
+        'agg': agg,
+        'total': movies.count(),
     })
 
 
-def show_one_movie(request, id_movie: int):
-    movie = get_object_or_404(Movie, id=id_movie)
+def show_one_movie(request, slug_movie: str):
+    movie = get_object_or_404(Movie, slug=slug_movie)
     return render(request, 'movie_app/one_movie.html', {
         'movie': movie
     })
