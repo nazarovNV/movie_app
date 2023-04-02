27a34007e9505726e95bd913bef2d715d2ef8f3d
Nikolay <nikolaynazarovvv216@gmail.com>
2023-04-02 19:32:23 +0300

    finished 3.16

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Movie
-from django.db.models import F, Sum, Max, Min, Count, Avg
+from django.db.models import F, Sum, Max, Min, Count, Avg, Value
 
 
 # from movie_app.models import Movie
@@ -9,7 +9,14 @@ from django.db.models import F, Sum, Max, Min, Count, Avg
 
 
 def show_all_movie(request):
-    movies = Movie.objects.order_by(F('year').asc(nulls_last=True), 'rating')
+    # movies = Movie.objects.order_by(F('year').asc(nulls_last=True), 'rating')
+    movies = Movie.objects.annotate(
+        true_bool=Value(True),
+        false_bool=Value(False),
+        str_field=Value("Hello"),
+        int_field=Value(123),
+        new_budget=F("budget")+100,
+                                    )
     agg = movies.aggregate(Avg("budget"), Max("rating"), Min("rating"), Max("year"))
     return render(request, 'movie_app/all_movies.html', {
         'movies': movies,
--- movie_project/settings.py
+++ movie_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'movie_app',
     'django_extensions',
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    "127.0.0.1:8000",
+    "localhost:8000",
+    # ...
 ]
 
 ROOT_URLCONF = 'movie_project.urls'
